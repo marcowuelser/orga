35198c522adc6dbb2966f3e6ba2c4d145684f3a3
Marco Wuelser <marco.wuelser@megaplex.ch>
2016-04-09 01:43:34 +0200

    add CORS

--- src/public/api/v1/index.php
+++ src/public/api/v1/index.php
@@ -87,6 +87,14 @@ if ($config["authenticationOn"])
 
 injectRoutes($app);
 
+// CORS
+$corsOptions = array(
+    "origin" => "*",
+    "exposeHeaders" => array("Content-Type", "X-Requested-With", "X-authentication", "X-client"),
+    "allowMethods" => array('GET', 'POST', 'PUT', 'DELETE', 'OPTIONS')
+);
+$cors = new \CorsSlim\CorsSlim($corsOptions);
+$app->add($cors);
 
 // Run App
 $app->run();
