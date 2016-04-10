a8bc6b2a20a49202151ea25ff3e7e916642b7636
Marco Wuelser <marco.wuelser@megaplex.ch>
2016-04-10 11:17:26 +0200

    Add scope enum

---
+++ src/classes/ScopeEnum.php
@@ -0,0 +1,58 @@
+<?php
+
+class ScopeEnum
+{
+    const ScopeUser = 1;
+    const ScopePlayer = 2;
+    const ScopeCharacter = 3;
+
+    public static function toString($value)
+    {
+        if (is_object($value))
+        {
+            return "Unknown";
+        }
+
+        switch ($value)
+        {
+            case ScopeEnum::ScopeUser:
+                return "User";
+            case ScopeEnum::ScopePlayer:
+                return "Player";
+            case ScopeEnum::ScopeCharacter:
+                return "Character";
+        }
+        return "Unknown";
+    }
+
+    public static function toList()
+    {
+        return array(
+            ScopeEnum::ScopeUser,
+            ScopeEnum::ScopePlayer,
+            ScopeEnum::ScopeCharacter,
+        );
+    }
+
+    public static function toArray()
+    {
+        $array = array();
+        foreach (ScopeEnum::toList() as $id)
+        {
+            $array[$id] = ScopeEnum::toString($id);
+        }
+        return $array;
+    }
+
+    public static function toAssocArray()
+    {
+        $array = array();
+        foreach (ScopeEnum::toList() as $id)
+        {
+            $array[] = array("id" => $id, "name" => ScopeEnum::toString($id));
+        }
+        return $array;
+    }
+}
+
+?>
---
+++ test/classes/ScopeEnumTest.php
@@ -0,0 +1,109 @@
+<?php
+require_once("src/classes/ScopeEnum.php");
+/**
+ * Unit test for the ScopeEnum class.
+ */
+
+/**
+ * Test case.
+ */
+class ScopeEnumTest extends PHPUnit_Framework_TestCase
+{
+    public function testValues()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        $this->assertEquals(1, ScopeEnum::ScopeUser);
+        $this->assertEquals(2, ScopeEnum::ScopePlayer);
+        $this->assertEquals(3, ScopeEnum::ScopeCharacter);
+    }
+
+    public function testToString()
+    {
+        // Arrange
+        $sut = new ScopeEnum();
+
+        // Act
+
+        // Assert
+        $this->assertEquals("User", $sut::toString(ScopeEnum::ScopeUser));
+        $this->assertEquals("User", $sut::toString(1));
+        $this->assertEquals("User", $sut::toString("1"));
+
+        $this->assertEquals("Player", $sut::toString(ScopeEnum::ScopePlayer));
+        $this->assertEquals("Player", $sut::toString(2));
+        $this->assertEquals("Player", $sut::toString("2"));
+
+        $this->assertEquals("Character", $sut::toString(ScopeEnum::ScopeCharacter));
+        $this->assertEquals("Character", $sut::toString(3));
+        $this->assertEquals("Character", $sut::toString("3"));
+
+        $this->assertEquals("Unknown", $sut::toString(""));
+        $this->assertEquals("Unknown", $sut::toString(null));
+        $this->assertEquals("Unknown", $sut::toString(-1));
+        $this->assertEquals("Unknown", $sut::toString(0));
+        $this->assertEquals("Unknown", $sut::toString(4));
+        $this->assertEquals("Unknown", $sut::toString(42));
+        $this->assertEquals("Unknown", $sut::toString($sut));
+    }
+
+    public function testList()
+    {
+        // Arrange
+        $sut = new ScopeEnum();
+
+        // Act
+        $list = $sut::toList();
+
+        // Assert
+        $this->assertEquals(3, count($list));
+        $this->assertEquals(ScopeEnum::ScopeUser, $list[0]);
+        $this->assertEquals(ScopeEnum::ScopePlayer, $list[1]);
+        $this->assertEquals(ScopeEnum::ScopeCharacter, $list[2]);
+    }
+
+    public function testArray()
+    {
+        // Arrange
+        $sut = new ScopeEnum();
+        $expected = array(
+            ScopeEnum::ScopeUser => "User",
+            ScopeEnum::ScopePlayer => "Player",
+            ScopeEnum::ScopeCharacter => "Character",
+        );
+
+        // Act
+        $array = $sut::toArray();
+
+        // Assert
+        $this->assertEquals(3, count($array));
+        $this->assertEquals($expected, $array);
+    }
+
+    public function testAssocArray()
+    {
+        // Arrange
+        $sut = new ScopeEnum();
+        $expectedUser = array(
+            "id" => ScopeEnum::ScopeUser,
+            "name" => "User");
+        $expectedPlayer = array(
+            "id" => ScopeEnum::ScopePlayer,
+            "name" => "Player");
+        $expectedCharacter = array(
+            "id" => ScopeEnum::ScopeCharacter,
+            "name" => "Character");
+
+        // Act
+        $array = $sut::toAssocArray();
+
+        // Assert
+        $this->assertEquals(3, count($array));
+        $this->assertEquals($array[0], $expectedUser);
+        $this->assertEquals($array[1], $expectedPlayer);
+        $this->assertEquals($array[2], $expectedCharacter);
+    }
+}
